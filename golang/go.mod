cd80060533e3164e8676e301687689792a464d18
zhulevich <154477663+zhulevich@users.noreply.github.com>
2024-07-10 16:49:45 +0300

    Add files via upload

---
+++ golang/go.mod
@@ -0,0 +1,5 @@
+module mymath
+
+go 1.22.4
+
+require github.com/zhulevich/golang1 v0.0.0-20240710125429-a35053371184 // indirect
---
+++ golang/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/zhulevich/golang1/mymath"
+)
+
+func main() {
+	x := 4.0
+	fmt.Println("корень из x будет ", mymath.Sqrt(x))
+}
